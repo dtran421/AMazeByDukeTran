8332463091385c433ce8ef0dd3f951bec7194da7
DukeTranWM <dtran@email.wm.edu>
2020-10-22 16:34:48 -0400

    tentatively finished UI for GeneratingActivity
    need to work on background image and progress bar

--- app/src/main/java/edu/wm/cs/cs301/duketran/gui/AMazeActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/duketran/gui/AMazeActivity.java
@@ -1,26 +1,76 @@
 package edu.wm.cs.cs301.duketran.gui;
 
 import edu.wm.cs.cs301.duketran.R;
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class AMazeActivity extends AppCompatActivity {
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+public class AMazeActivity extends Activity {
+    private static final int KEY_GENERATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_title);
+
+        Log.v("App Launch", "Successful");
 
         Spinner builderSpinner = findViewById(R.id.builderSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.builder, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         builderSpinner.setAdapter(adapter);
 
-        Spinner driverSpinner = findViewById(R.id.driverSpinner);
-        adapter = ArrayAdapter.createFromResource(this, R.array.driver, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        driverSpinner.setAdapter(adapter);
+        Button exploreButton = findViewById(R.id.exploreButton);
+        exploreButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                SeekBar skillLevelSeekBar = findViewById(R.id.seekBar);
+                int skillLevel = skillLevelSeekBar.getProgress();
+
+                Spinner builderSpinner = findViewById(R.id.builderSpinner);
+                String builder = builderSpinner.getSelectedItem().toString();
+
+                SwitchMaterial roomSwitch = findViewById(R.id.roomSwitch);
+                boolean rooms = roomSwitch.isChecked();
+
+                Intent mazeGeneration = new Intent(AMazeActivity.this, GeneratingActivity.class);
+                mazeGeneration.putExtra("SkillLevel", skillLevel);
+                mazeGeneration.putExtra("Builder", builder);
+                mazeGeneration.putExtra("Rooms", rooms);
+
+                Toast toast = Toast.makeText(AMazeActivity.this, "Generating maze...", Toast.LENGTH_SHORT);
+                toast.show();
+                startActivityForResult(mazeGeneration, KEY_GENERATION);
+            }
+        });
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == KEY_GENERATION)
+            if (resultCode == Activity.RESULT_OK) {
+                String driver = data.getExtras().get("Driver").toString();
+                String robot = data.getExtras().get("Robot").toString();
+                Log.v("Inputted Driver", driver);
+                Log.v("Inputted Robot", robot);
+
+                Toast toast = Toast.makeText(AMazeActivity.this, "Loading game...", Toast.LENGTH_SHORT);
+                toast.show();
+                //startActivity(mazeGeneration, KEY_GENERATION);
+            }
+            else if (resultCode == Activity.RESULT_CANCELED) {
+                Log.e("Maze Generation", "Failed!");
+            }
+    }
+
+
 }
---
+++ app/src/main/java/edu/wm/cs/cs301/duketran/gui/GeneratingActivity.java
@@ -0,0 +1,99 @@
+package edu.wm.cs.cs301.duketran.gui;
+
+import edu.wm.cs.cs301.duketran.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+
+public class GeneratingActivity extends Activity implements Runnable {
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generating);
+
+        handler = new Handler();
+        Thread generationThread = new Thread(this);
+        generationThread.start();
+
+        Spinner driverSpinner = findViewById(R.id.driverSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.driver, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        driverSpinner.setAdapter(adapter);
+
+        Spinner robotSpinner = findViewById(R.id.robotSpinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.robot, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        robotSpinner.setAdapter(adapter);
+
+        driverSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                toggleRobotLayout(parent.getItemAtPosition(position).toString());
+            }
+            public void onNothingSelected(AdapterView<?> parent) {
+                toggleRobotLayout(parent.getSelectedItem().toString());
+            }
+        });
+
+        Button playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Spinner driverSpinner = findViewById(R.id.driverSpinner);
+                String driver = driverSpinner.getSelectedItem().toString();
+
+                Spinner robotSpinner = findViewById(R.id.robotSpinner);
+                String robot = robotSpinner.getSelectedItem().toString();
+
+                Intent result = new Intent(GeneratingActivity.this, AMazeActivity.class);
+                result.putExtra("Driver", driver);
+                result.putExtra("Robot", robot);
+                GeneratingActivity.this.setResult(RESULT_OK, result);
+                GeneratingActivity.this.finish();
+            }
+        });
+    }
+
+    private void toggleRobotLayout(String selectedItem) {
+        LinearLayout robotLayout = findViewById(R.id.robotLayout);
+        if(!selectedItem.equals("Wallfollower")) robotLayout.setVisibility(View.INVISIBLE);
+        else robotLayout.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void run() {
+        Intent intent = getIntent();
+        Log.v("SkillLevel", ""+intent.getIntExtra("SkillLevel", 0));
+        Log.v("Builder", ""+intent.getStringExtra("Builder"));
+        Log.v("Rooms", ""+intent.getBooleanExtra("Rooms", false));
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        int currentProgress = 0;
+        while (currentProgress < 100) {
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                setResult(RESULT_CANCELED, null);
+                finish();
+                return;
+            }
+            currentProgress += 15;
+            progressBar.setProgress(currentProgress);
+        }
+
+        handler.post(new Runnable() {
+            public void run() {
+                Button playButton = findViewById(R.id.playButton);
+                playButton.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
